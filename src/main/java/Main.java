75514d6097b039253e21431c34cf856cffb5dd0d
Santos <santos@Zipcoders-MacBook-Pro-9.local>
2023-06-27 15:56:07 -0400

    I have added changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        
+        Random rd = new Random();
+        int num=101;
+        int rand=(rd.nextInt(100));
+        Scanner in= new Scanner(System.in);
+
+               while(num != rand){
+
+                   System.out.println("select your number 1-100");
+                   num = in.nextInt();
+                   if(num<rand){
+                       System.out.println("too small");
+                   }
+                   else if (num>rand){
+                       System.out.println("too big");
+                   }
+
+               }
+        System.out.println("Correct the number was "+rand);
+
     }
 }
