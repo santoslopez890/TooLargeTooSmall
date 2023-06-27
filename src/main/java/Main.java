32e1d2251f21a527fb6f25e7fa8123a400e1a794
Santos <santos@Zipcoders-MacBook-Pro-9.local>
2023-06-27 17:26:54 -0400

    finished product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,31 @@ public class Main {
     public static void main(String[] args){
         Random rd = new Random();
         int num=101;
+        int oldGuess=0;
+        int counter=0;
         int rand=(rd.nextInt(100));
         Scanner in= new Scanner(System.in);
+        System.out.println("select your number 1-100");
+               while(num != rand) {
 
-               while(num != rand){
 
-                   System.out.println("select your number 1-100");
                    num = in.nextInt();
-                   if(num<rand){
+
+                   if (oldGuess == num) {
+                    System.out.println("printed same num");
+                       continue;
+                   } else if (num < rand) {
                        System.out.println("too small");
-                   }
-                   else if (num>rand){
+                   } else if (num > rand) {
                        System.out.println("too big");
                    }
+                   oldGuess = num;
+                   counter++;
+
+
 
                }
-        System.out.println("Correct the number was "+rand);
+        System.out.println("Correct the number was "+rand+" it took you "+counter+" guesses");
 
     }
 }
